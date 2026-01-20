03119e9aadf80ade453b2e9ee14c4dd7ad3b0cc2
Akshat Nigam <nigamakshat29jan2000@gmail.com>
2026-01-20 18:24:11 +0530

    setted up one-key backend

---
+++ one-key-backend/src/main/java/com/projects/auth/one_key_backend/OneKeyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projects.auth.one_key_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneKeyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneKeyBackendApplication.class, args);
+	}
+
+}
---
+++ one-key-backend/src/test/java/com/projects/auth/one_key_backend/OneKeyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.auth.one_key_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneKeyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
